bb8ec6a608b7257a09609f44abb6ca012baaeb3a
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-24 14:44:08 +0530

    discuss about operators

---
+++ Basic/operators.js
@@ -0,0 +1,32 @@
+// Arithmetic operators
+/*
+5+5
+10-5
+5*2
+10/3
+10%2
+2**3 => 2*2*2
+*/
+
+let value = 3
+value++
+++value
+
+const numberOne = "2"
+const numberTwo = "4"
+
+const numberThree = numberOne + numberTwo
+console.log(numberThree)
+
+// 2+(2-(3*4))/4+2
+
+//Assignment Operators
+
+let num = 3
+num += 10
+console.log("number is :"+ num)
+
+//Comparision Operators
+let a=3     //number
+let b= "3"  //string
+console.log(a === b)

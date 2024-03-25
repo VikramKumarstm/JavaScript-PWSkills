0d8ab99f861d927256527c78e4e7107122755e40
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-25 11:10:26 +0530

    About String

---
+++ Basic/Num_String.js
@@ -0,0 +1,32 @@
+const firstName = "vikram"
+const lastName = "kumar"
+
+console.log(firstName +' '+ lastName)
+console.log(`My first name is ${firstName} and last name is ${lastName}`)
+
+const userName = new String('vikramdotcom')
+console.log(userName)
+console.log(userName[0])
+
+//find length
+console.log(userName.length)
+
+console.log(userName.charAt(0))
+console.log(userName.substring(0, 6))
+
+let email = '  v@hc.com   '
+console.log(email)
+// console.log(email.trimStart())
+// console.log(email.trimEnd())
+console.log(email.trim())
+
+console.log(email.replace('v', 'vikram'))
+
+console.log(email.toUpperCase())
+console.log(email.toLowerCase())
+
+
+//Assignment
+/*
+1. Learn more about string and string method
+ */

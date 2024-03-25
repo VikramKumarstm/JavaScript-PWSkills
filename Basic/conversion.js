7c70c16547bdd461df8cbc8b6822933d33fcc422
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-25 10:24:14 +0530

    about type conversion

---
+++ Basic/conversion.js
@@ -0,0 +1,16 @@
+let bankBalance = '100'
+
+let bankBalanceinInt = parseInt(bankBalance)  //Number(bankBalance)
+
+// console.log(bankBalance)
+// console.log(bankBalanceinInt)
+
+console.log(typeof bankBalance);  //string
+console.log(typeof bankBalanceinInt); //Number
+
+console.log(2 == 2)     //true
+console.log(NaN == NaN) // false
+
+console.log(5+NaN)    //NaN
+console.log(5+undefined) // NaN
+console.log(5+null)      //5

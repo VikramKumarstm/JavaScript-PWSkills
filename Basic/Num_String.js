8ae8b0ca332a6159289cf9c3540f6da2e1932804
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-25 13:56:30 +0530

    about Number

--- Basic/Num_String.js
+++ Basic/Num_String.js
@@ -30,3 +30,40 @@ console.log(email.toLowerCase())
 /*
 1. Learn more about string and string method
  */
+
+console.log("********************************************")
+
+//Number
+
+const number = 5;
+
+const num = new Number(5.27112)
+// console.log(num.toString())
+console.log(num.toFixed(2))
+
+const subscribers = 100000
+console.log(subscribers.toLocaleString())
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+
+/**
+ * () -> prentheses
+ * [] -> brackets
+ * {} -> curly braces
+ */
+
+
+console.log(Math)
+console.log(Math.random())
+console.log(Math.random().toFixed(2))
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random()*10))
+
+//Assignments
+/**
+ * study about Math() method
+ * generate 1 to 6 random number
+ */
+

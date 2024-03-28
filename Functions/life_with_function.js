525380db4b6915ec1b0ad5562d87a931d907df65
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-28 08:48:04 +0530

    Life with function

---
+++ Functions/life_with_function.js
@@ -0,0 +1,15 @@
+function calculateSquare(num) {
+    // logic to calculate square of a number
+
+    let squareValue = num * num;
+
+    // return the out of the square value
+
+    return squareValue;
+}
+
+let result1 = calculateSquare(5);
+console.log(result1);
+
+console.log(calculateSquare(9));
+

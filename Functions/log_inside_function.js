dac15ae26d2e1abe87f7627ae28f84002f563d23
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-29 09:19:00 +0530

    log inside function

---
+++ Functions/log_inside_function.js
@@ -0,0 +1,14 @@
+function calculateSquare(num) {
+    // logic to calculate square of a number
+
+    let squareValue = num * num;
+
+    console.log(squareValue); //instead of return we did a log
+}
+
+calculateSquare(10);  //here 10 is an argument
+
+let result1 = calculateSquare(5);
+console.log(result1);
+
+console.log(calculateSquare(9));

feec2e683fb9f321d94e86028ba0df21c53c30a0
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-30 08:35:07 +0530

    Named function Expression

---
+++ Functions/named_function_expression.js
@@ -0,0 +1,11 @@
+const isEvenOrOdd = function process(num) {
+    return num%2 == 0;
+}
+
+//How to call it?
+const result = isEvenOrOdd(150);
+console.log(result);
+
+//Q. is this a named function expression?
+//-> yes.
+

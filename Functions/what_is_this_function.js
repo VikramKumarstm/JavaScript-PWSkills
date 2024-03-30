5b4829d92ff92f5b78928263d9d88fb83ba40fb6
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-30 08:34:05 +0530

    Types of Functions

---
+++ Functions/what_is_this_function.js
@@ -0,0 +1,29 @@
+//expression :
+/**
+ * if the first valid word is not function in the statement
+ * then it is function expression
+ */
+
+function fun() { //function declaration
+
+}
+
+const f = function process() {  // named fuction expression
+
+}
+
+const g = function () {  // anonymous fuction expression
+
+}
+
+const h = () => {  //fuction expression
+
+}
+
+(function () {  //fuction expression
+
+})()
+
+(function exec() {  ////fuction expression
+
+})()

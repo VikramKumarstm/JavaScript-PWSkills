5a71afb0d9eab2fde1e7f84b033678c2a72cf816
Vikram Kumar <vikramclg0000@gmail.com>
2024-03-31 09:35:01 +0530

    About Call Stack

---
+++ Functions/callstack_demo.js
@@ -0,0 +1,26 @@
+function f1() {
+    console.log("inside f1");
+}
+
+function f2() {
+    console.log("inside f2");
+    f1();
+    return 7+8;
+}
+
+function f3() {
+    console.log("inside f3");
+    f2();
+    return 5;
+}
+
+function f4() {
+    console.log("inside f4");
+    const x = 10;
+    const y = f3();
+    return x+y;
+}
+
+console.log("start");
+const result = f4();
+console.log(result);

9c2b79a79bb869627f0624af4445d2cade04e337
Vikram Kumar <vikramclg0000@gmail.com>
2024-04-03 10:38:15 +0530

    Life without IIFE function

---
+++ Functions/life_without_iife.js
@@ -0,0 +1,13 @@
+function downloadScriptFromIdeoneAndPopulateDatabase() {
+    console.log("Old function implementation.");
+}
+
+downloadScriptFromIdeoneAndPopulateDatabase();
+
+function downloadScriptFromIdeoneAndPopulateDatabase() {
+    console.log("New function created");
+}
+
+console.log("calling intern code");
+downloadScriptFromIdeoneAndPopulateDatabase();
+

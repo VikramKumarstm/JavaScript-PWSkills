fedc94d660c38241a563e07d85f7d1775066cb22
Vikram Kumar <vikramclg0000@gmail.com>
2024-04-06 12:37:50 +0530

    Discuss about scope - 1

---
+++ Scope/demo.js
@@ -0,0 +1,14 @@
+var teacher = "Vishva Mohan"  // global scope
+
+function fun() {   //global scope
+    var teacher = "Sanket Singh"  //function fun scope
+    console.log("hello", teacher)
+}
+
+function gun() {  // global scope
+    var student = "Amit"  // function gun scope
+    console.log("Welcome to the course", student);
+}
+
+fun()
+gun()

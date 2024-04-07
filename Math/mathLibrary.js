e9affe514321822aae12f196b94562132874b07d
Vikram Kumar <vikramclg0000@gmail.com>
2024-04-07 15:47:56 +0530

    Math function

---
+++ Math/mathLibrary.js
@@ -0,0 +1,37 @@
+// abs
+// abs(positive_value) -> positive_value
+// abs(negative_value) -> positive_counterpart_of that negative value
+// |x| -> Ex: |10| -> 10, |-8| -> 8 
+
+console.log(Math.abs(-9));
+console.log(Math.abs(8));
+
+let a = 's';
+let b = Number(a);
+console.log(Math.abs(b));
+
+// Round off ->
+
+console.log(Math.round(14.6));
+
+// floor -> it returns first integer just less than or equal to x
+
+console.log(Math.floor(4.3))
+console.log(Math.floor(7.99))
+console.log(Math.floor(4))
+
+// ceil -> it helps you to find, first integer greater than or equal to x
+
+console.log(Math.ceil(4.1));
+console.log(Math.ceil(8.9));
+console.log(Math.ceil(3.01));
+
+// Max and Min
+
+console.log(Math.max(8,9,121,15,1,7, 121));
+console.log(Math.min(9, 6, 0, 8, 9, -2));
+
+// sqrt -> square root
+
+console.log(Math.sqrt(100))
+

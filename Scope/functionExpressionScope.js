eddcce247feceddb87446691c70b6f2618b9e0e8
Vikram Kumar <vikramclg0000@gmail.com>
2024-04-07 10:00:49 +0530

    function expression scope

---
+++ Scope/functionExpressionScope.js
@@ -0,0 +1,11 @@
+const fun = function f() {
+    var y = 10;
+    console.log(y);
+}
+
+f();
+
+/**
+ * the whole function of 'f()' is in the fun scope 
+ * that's why function f() is not accessible outside;
+ */

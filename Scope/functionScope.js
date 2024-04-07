032d046cbd87eb22505e9d5301abe6f425b2089f
Vikram Kumar <vikramclg0000@gmail.com>
2024-04-07 10:01:28 +0530

    about TDZ

--- Scope/functionScope.js
+++ Scope/functionScope.js
@@ -11,3 +11,12 @@ if(true) {
     console.log(y);
 }
 
+/**
+ * Although y has a block scope & here this 'if' is
+ * a block, we still cannot access y, before the line
+ * of its declaration as before that line it is a TDZ
+ * for y.
+ * 
+ * TDZ -> Temporal Dead Zone
+ */
+
